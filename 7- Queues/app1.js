075ad73fc0799e74fbb751bcee5ed34bf79b9e18
AbidaAli100 <abida.ali100@gmail.com>
2021-07-16 18:36:14 +0500

    practiced queue method

--- 7- Queues/app1.js
+++ 7- Queues/app1.js
@@ -1,6 +1,6 @@
 class Queue {
     constructor() {
-        this.item = ["ali"];
+        this.item = ["ali", "Fahad"];
         this.size = 5;
     }
     enqueue(item) {
@@ -11,9 +11,15 @@ class Queue {
         this.item.shift();
         this.size--;
     }
-
+    dequeueCustomMethod() {
+        for (let i = 0; i < this.item.length - 1; i++) {
+            this.item[i] = this.item[i + 1];
+            console.log(this.item[i]);
+        }
+    }
 }
 let myQueue = new Queue();
 myQueue.enqueue("Abida");
 myQueue.dequeue();
+myQueue.dequeueCustomMethod();
 console.log(myQueue);
